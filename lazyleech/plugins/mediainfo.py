c9f4d270de70714724f42fa74bf99adba20b1411
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-02-16 22:57:07 +0500

    Update mediainfo.py

--- lazyleech/plugins/mediainfo.py
+++ lazyleech/plugins/mediainfo.py
@@ -72,7 +72,7 @@ async def mediainfo(client, message):
     if x_media is None:
        await process.edit_text("Reply To a Valid Media Format")
        return
-    media_type = reply.video.file_name
+    media_type = str(type(x_media)).split("'")[1]
     file_path = safe_filename(await reply.download())
     output_ = await runcmd(f'mediainfo "{file_path}"')
     out = None
